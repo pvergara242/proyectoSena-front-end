72096ccf60d6d91e8473e32ed9a86e6228e22df3
Hansel Junior Ospino Orellano <hjoo0111@gmail.com>
2020-07-19 13:45:14 -0500

    feature - pagar factura: Pagar factura implementado. Detalles de factura pagada implementado.

--- src/routes/factura.routes.js
+++ src/routes/factura.routes.js
@@ -5,11 +5,13 @@
     
 //crud inventarios 
     const {
-        renderFactura
+        renderFactura,
+        renderPagarFactura
     } = require('../controllers/factura.controller');
 
 //metodo post envia los datos en este caso de inventarios
     router.post('/Factura', renderFactura);
+    router.post('/Factura/Pagar', renderPagarFactura);
 
 //le estamos diciendo al enrutador que use esa funcion para cualquier llamada que haga 
     router.use(auth.secureEndpoints);
